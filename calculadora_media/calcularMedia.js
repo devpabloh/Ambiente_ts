d6c69b5945897d61fc8661b6c55918a33b2ee3cf
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-31 23:02:04 -0300

    Update calcularMedia.js

--- calculadora_media/calcularMedia.js
+++ calculadora_media/calcularMedia.js
@@ -4,8 +4,19 @@ function calcularMedia(){
     let notas = document.querySelectorAll(".notas")
     let soma = 0
     let quantidade = notas.length
+    
+    (function calcularMedia(){
+        notas.forEach(function(notas){
+            let valor = parseFloat(notas.value)
+            if (isNaN(valor)){
+                soma += valor
+            }
+        });
+
+        let media = soma / quantidade
+    
+        document.getElementById("").textContent = media
+    })
 }
 
-(function calcularMedia(){
-    
-})
+
