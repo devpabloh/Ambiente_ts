5e43e86e49212cd29bee387cc64f1fa2c7e7321c
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-31 22:29:06 -0300

    atualização

---
+++ calculadora_media/calcularMedia.js
@@ -0,0 +1,11 @@
+
+function calcularMedia(){
+    // Primeiro vamos obter as informações das notas
+    let notas = document.querySelectorAll(".notas")
+    let soma = 0
+    let quantidade = notas.length
+}
+
+(function calcularMedia(){
+    
+})
