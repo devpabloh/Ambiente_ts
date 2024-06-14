a4d96133637245c49ef7082eff0a746db4765c04
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-14 10:44:56 -0300

    atualização

---
+++ ajudandoPrograMaria/teste.js
@@ -0,0 +1,7 @@
+function validarFormulario(){
+    if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+        alert("Prontinho! Você receberá as novidades por email.")
+    }else{
+        alert("Por favor, preencha os campos nome e email!")
+    }
+}
